faab49f1928c06cd6c72518a96b7492802b3fbaf
gool <ss22219@users.noreply.github.com>
2019-01-19 17:21:52 +0800

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,1 +1,2 @@
-docker exec  `docker ps | grep dotnet2.2_perf | awk '{print $1}'` /bin/bash
+docker exec -it --privileged=true `docker ps | grep dotnet2.2_perf | awk '{print $1}'` /perfcollect install
+docker exec --privileged=true `docker ps | grep dotnet2.2_perf | awk '{print $1}'` /bin/bash
